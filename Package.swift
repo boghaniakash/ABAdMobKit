6423ec73e6edbab43926cdcee43d58e650eca8d5
Akash <akashboghani111@gmail.com>
2025-01-07 18:04:21 +0530

    - first commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ABAdMobKit",
+    products: [
+        .library(
+            name: "ABAdMobKit",
+            targets: ["ABAdKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", .upToNextMajor(from: "11.13.0"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ABAdBinaryKit",
+            path: "Sources/Frameworks/ABAdKit.xcframework"
+        ),
+        .target(
+            name: "ABAdKit",
+            dependencies: [
+                .target(name: "ABAdBinaryKit"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ],
+            path: "Sources/ABAdKit"
+        )
+    ]
+)
---
+++ Sources/Frameworks/ABAdKit.xcframework/ios-arm64_x86_64-simulator/ABAdKit.framework/Headers/ABAdKit.h
@@ -0,0 +1,18 @@
+//
+//  ABAdKit.h
+//  ABAdKit
+//
+//  Created by Akash Boghani on 07/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ABAdKit.
+FOUNDATION_EXPORT double ABAdKitVersionNumber;
+
+//! Project version string for ABAdKit.
+FOUNDATION_EXPORT const unsigned char ABAdKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABAdKit/PublicHeader.h>
+
+
