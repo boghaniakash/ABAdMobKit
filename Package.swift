3003cc0fb9d677a6c0633e71ba37211358a33869
Akash Boghani <akashboghani111@gmail.com>
2025-03-08 19:57:30 +0530

    - Add Other frameworks

--- Package.swift
+++ Package.swift
@@ -7,8 +7,25 @@ let package = Package(
     name: "ABAdMobKit",
     products: [
         .library(
-            name: "ABAdMobKit",
-            targets: ["ABAdKit", "ABAdKitTarget"]),
+            name: "ABAdIOS",
+            targets: ["ABAdIOS", "ABAdKitTarget"]
+        ),
+        .library(
+            name: "AB2CommonIOS",
+            targets: ["AB2CommonIOS"]
+        ),
+        .library(
+            name: "AB2NetworkIOS",
+            targets: ["AB2NetworkIOS"]
+        ),
+        .library(
+            name: "AB2RouterIOS",
+            targets: ["AB2RouterIOS"]
+        ),
+        .library(
+            name: "AB2WrapperIOS",
+            targets: ["AB2WrapperIOS"]
+        )
     ],
     dependencies: [
         .package(
@@ -18,13 +35,29 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "ABAdKit",
+            name: "ABAdIOS",
             path: "Sources/Frameworks/ABAdKit.xcframework"
+        ),
+        .binaryTarget(
+            name: "AB2CommonIOS",
+            path: "Sources/Frameworks/AB2CommonIOS.xcframework"
+        ),
+        .binaryTarget(
+            name: "AB2NetworkIOS",
+            path: "Sources/Frameworks/AB2NetworkIOS.xcframework"
+        ),
+        .binaryTarget(
+            name: "AB2RouterIOS",
+            path: "Sources/Frameworks/AB2RouterIOS.xcframework"
+        ),
+        .binaryTarget(
+            name: "AB2WrapperIOS",
+            path: "Sources/Frameworks/AB2WrapperIOS.xcframework"
         ),
         .target(
             name: "ABAdKitTarget",
             dependencies: [
-                .target(name: "ABAdKit"),
+                .target(name: "ABAdIOS"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
             ],
             path: "Sources/ABAdKit"
